75d309d97238c5a93647ae89aa62f0ed27f27124
Jennifer Miller <jmiller@archersystems.us>
2019-12-13 02:49:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,41 +1,41 @@
+require "pry"
+
+
 class CashRegister
+  attr_accessor :total, :discount, :items, :items_with_prices
 
-  attr_accessor :total, :discount, :last_transaction_amount, :items
-
-  def initialize(discount = 0)
+  def initialize(employee_discount=0)
     @total = 0
-    @discount = discount
+    @discount = employee_discount
     @items = []
+    @items_with_prices = []
   end
 
-  def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
+  def add_item(title, price, quantity=1)
+    quantity.times do
       @items << title
     end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total
+    charge_total = quantity * price
+    @total += charge_total
+    @items_with_prices << {title: title, price: price}
   end
 
-  def apply_discount()
+  def apply_discount
     if @discount > 0
-      @discount = @discount/100.to_f
-      @total = @total - (@total * (@discount))
+      @total = @total.to_f * ((100 - @discount.to_f ) / 100)
       "After the discount, the total comes to $#{@total.to_i}."
     else
       "There is no discount to apply."
     end
   end
 
-
-  def void_last_transaction()
-    @total -= @last_transaction_amount
+  def void_last_transaction
+    last_item = @items_with_prices.last
+    @total = @total - last_item[:price]
+    @items_with_prices.pop
+    if @items_with_prices.empty?
+      @total = 0
+    end
   end
 
 end
